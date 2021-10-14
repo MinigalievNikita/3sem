f7179efc91ad17be25b9894fd520cb0edbeb714a
MinigalievNikita <70330429+MinigalievNikita@users.noreply.github.com>
2021-10-14 21:45:57 +0300

    Update and rename f to messages_by_threads.c

--- messages_by_threads.c
+++ messages_by_threads.c
@@ -7,35 +7,43 @@
 #include <time.h>
 #include <pthread.h>
 
-
+const int SIZE_OF_BUFFER = 1000;
 
 void *writer(void *arg){
     int index;
-    char *fifo2 = (char*) arg;
-    char *buffer2 = calloc(1000, sizeof(char));
-    index = open(fifo2, O_WRONLY);
+    char *fifo = (char*) arg;
+    char *buffer = calloc(SIZE_OF_BUFFER, sizeof(char));
+    index = open(fifo, O_WRONLY);
+    
     while(1) {
-        gets(buffer2);
-        write(index, buffer2, 1000);
+        gets(buffer);
+        write(index, buffer, SIZE_OF_BUFFER);
     }
-    free (buffer2);
+    free (buffer);
 }
 
-
+void reader(char *fifo, char *buffer) {
+    int index;
+    index = open(fifo, O_RDONLY);
+    while(1) {
+    read(index, buffer , SIZE_OF_BUFFER);
+    printf("%s\n", buffer);
+    }
+}
 
 int main() {
-        char* fifo12 = "FIFO1.txt";
+    char* fifo12 = "FIFO1.txt";
     char* fifo21 = "FIFO2.txt";
     mknod(fifo12, S_IFIFO | 0666,0);
     mknod(fifo21, S_IFIFO | 0666,0);
     pthread_t thid;
-    char *buffer = calloc(1000, sizeof(char));
+    char *buffer = calloc(SIZE_OF_BUFFER, sizeof(char));
     int arg;
     printf("ENTER USER's NUMBER (1 or 2):");
     scanf("%d", &arg);
     int result;
+    
     if(arg == 1) {
-        int index;
 
         result = pthread_create( &thid, (pthread_attr_t *)NULL, writer, fifo12);
 
@@ -43,15 +51,9 @@ int main() {
             printf("ERROR");
         }
 
-        index = open(fifo21, O_RDONLY);
-        while(1) {
-        read(index, buffer , 1000);
-        printf("%s\n", buffer);
-        }
-
+        reader(fifo21, buffer);
 
     } else {
-        int index;
 
         result = pthread_create( &thid, (pthread_attr_t *)NULL, writer, fifo21);
 
@@ -59,16 +61,10 @@ int main() {
             printf("ERROR");
         }
 
-        index = open(fifo12, O_RDONLY);
-        while(1) {
-        read(index, buffer , 1000);
-        printf("%s\n", buffer);
-        }
-
+        reader(fifo12, buffer);
 
     }
     free (buffer);
     return 0;
 }
-
 
